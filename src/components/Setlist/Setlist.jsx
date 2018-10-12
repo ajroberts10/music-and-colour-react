1f2860a16f9fcbd2288bed39f93e729edb050b84
Adam Roberts <adam.roberts@bbc.co.uk>
2018-10-12 20:28:56 +0100

    adding setlist component and making searchable

---
+++ src/components/Setlist/Setlist.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import SetlistItem from './SetlistItem';
+import './Setlist.scss';
+
+class Setlist extends Component {
+    constructor() {
+        super();
+        this.state = { setlistItems: undefined };
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleSearch(event, setlist) {
+        const searchQuery = event.target.value.toLowerCase();
+        const searchResults = setlist.filter((item) => {
+            const titleMatch = item.title.toLowerCase();
+            const artistMatch = item.artist.toLowerCase();
+            return titleMatch.indexOf(searchQuery) !== -1 || artistMatch.indexOf(searchQuery) !== -1;
+        });
+        this.setState({
+            setlistItems: searchResults
+      })
+    }
+
+    render() {
+        return (
+        <Query query={gql`
+            {
+                setlist {
+                    title,
+                    artist
+                }
+            }
+        `}>
+            {({loading, error, data: { setlist }}) => {
+                const setlistItems = this.state.setlistItems || setlist;
+                if (loading) return <p>Loading ...</p>;
+                if (error) return <p>Error ...</p>;
+                return (
+                    <div className="setlist">
+                        <div className="container">
+                            <div className="row">
+                                <input
+                                    className="form-control form-control-lg setlist__search"
+                                    type="text"
+                                    onChange={(e) => {this.handleSearch(e, setlist)}}
+                                    placeholder="Search for song"
+                                />
+                            </div>
+                            <div className="row">
+                                <ul className="setlist__list">
+                                    {
+                                        setlistItems.map(item => <SetlistItem key={item.title} itemData={item} />)
+                                    }
+                                </ul>
+                            </div>
+
+
+                        </div>
+
+
+                </div>
+                )}}
+        </Query>
+    )}
+};
+
+export default Setlist;
---
+++ src/components/Setlist/SetlistItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './SetlistItem.scss'
+
+const SetlistItem = ({itemData : { title, artist }}) => (
+    <li>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <span className=" setlistItem__title">{title}</span>
+                </div>
+                <div className="col">
+                    <span className="setlistItem__artist">{artist}</span>
+                </div>
+
+            </div>
+        </div>
+    </li>
+);
+
+export default SetlistItem;
